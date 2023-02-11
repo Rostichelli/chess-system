26a341c31c9a5197e004159a05ead8ac75472bbe
ROSTICHELLI <leskalves.douglas@gmail.com>
2023-02-11 18:13:25 -0300

    Applying defensive programming in the board class

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,7 +23,8 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(1, 0));
 	}
 
 }
